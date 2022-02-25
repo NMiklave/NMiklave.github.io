2437a5bfc09c115aa15e0a485c8c35016ccbd13d
NMiklave <50243212+NMiklave@users.noreply.github.com>
2022-02-24 18:56:56 -0600

    Test bugfix for collapsable blog entries

--- assets/collapsableBlog.js
+++ assets/collapsableBlog.js
@@ -2,22 +2,17 @@
 To use: Make a list, in each <li> element, include a <button> (to be clicked on) and a <p> that will have its display toggled.
 */
 
-// Get all elements of type <button>
-var allSpan = document.getElementsByClassName("CollapsableItem");
-for(var x = 0; x < allSpan.length; x++) {
-	// Listen for clicks to occur, then execute
-  allSpan[x].addEventListener("click", function() {
-  	// Toggle the .active class (starts off)
-  	this.classList.toggle("active");
 
-    // Get all <p> elements that are sibling to button
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    	content.style.padding = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-			content.style.padding = "5px 20px 10px";
-    }
-  });
+function collapsable(entry) {
+	// Toggle the .active class (starts off)
+  entry.classList.toggle("active");
+  // Look at the next sibling element to the button
+  var content = entry.nextElementSibling;
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+    content.style.padding = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+    content.style.padding = "5px 20px 10px";
+  }
 }
